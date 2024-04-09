2ebac5483a8b3ae6b3ebc1c9caee63b83d4221e7
Raheel-Iqbal83287 <rhiqbl@gmail.com>
2024-04-09 14:01:30 +0500

    First Commit

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const num = Math.floor(Math.random() * 6 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name: "guess",
+        type: "number",
+        message: "Please guess a number from 1 to 6 : ",
+    }
+]);
+
+if(answer.guess === num){
+    console.log("Congratulations! You guess the right number.")
+}    
+else{
+    console.log("Sorry! You have not guessed the number.")
+}
